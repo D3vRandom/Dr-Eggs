67129a857dd5ae07a18691e8f931a5c93defdbfb
D3vRandom <devr4ndom@gmail.com>
2020-08-13 01:10:02 -0400

    Modified Jackit Code Base

--- examples/barbiegirl.sh
+++
@@ -1,2 +0,0 @@
-# Inspired by: https://github.com/EmpireProject/Empire/blob/master/lib/modules/python/trollsploit/osx/thunderstruck.py
-osascript -e "set Volume 100" && open -a "Safari" -g -j 'https://youtu.be/ZyhrYis509A?t=13' && exit
